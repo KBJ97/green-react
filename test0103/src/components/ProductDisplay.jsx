a3394899053ee7c55d0e574459a3e9b6bdb94c2a
KBJ97 <https://github.com/KBJ97/KBJ-git.git~>
2023-01-04 15:57:30 +0900

    애플리케이션 과제

--- test0103/src/components/ProductDisplay.jsx
+++ test0103/src/components/ProductDisplay.jsx
@@ -1,13 +1,12 @@
 const ProductDisplay = ({name, img}) => {
     return ( 
-        <div style={
-            {width:"300px", 
+        <div style={{
+            width:"300px", 
             height:"300px", 
             backgroundColor:"lightgrey",
             display: "inline-block",
             margin : "0 10px"
-            }
-        }>
+            }}>
             <img src={require(`../img/${img}`)} height="200" alt="물건사진" />
             <h3>{name}</h3>
         </div>
