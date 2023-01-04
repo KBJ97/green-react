1fedf6cb1740c24b60306e64d24b8ee55a6be3f1
KBJ97 <https://github.com/KBJ97/KBJ-git.git~>
2023-01-04 15:53:09 +0900

    프로젝트

---
+++ study-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EventOBJ from './components/EventOBJ';
+
+function App() {
+  return (
+    <div className="App">
+      <EventOBJ/>
+    </div>
+  );
+}
+
+export default App;
